0f69ef0e43876316329cc51197a1377e33f7ada9
Bohdan <rogozyanskiy.bogdan@gmail.com>
2024-03-26 00:57:37 +0200

    The number of projects localstorage done

--- submit_manager.php
+++ submit_manager.php
@@ -7,14 +7,14 @@ $pr_manager = $_GET["pr_manager"];
 $query = [];
 $query["manager"] = $pr_manager;
 
-$count = $collection_projects->count($query);
-
 $cursor = $collection_projects->find($query, ["_id" => 0]);
 
 $projects = [];
 foreach ($cursor as $project) {
     $projects[] = $project;
 }
+
+$currentFile = $_SERVER["PHP_SELF"];
 ?>
 
 <!DOCTYPE html>
@@ -24,10 +24,70 @@ foreach ($cursor as $project) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo($pr_manager)?></title>
     <link rel="stylesheet" href="./css/response.css">
+    <script>
+        const saveToLocalStorage = () => {
+            const key = <?php echo("'$currentFile,$pr_manager'")?>;
+            const data = <?php echo(json_encode($projects))?>;
+            if(data.length != 0){
+                localStorage.setItem(key, JSON.stringify(data));
+            }
+        }
+        
+        const showSavedResults = (key) => {
+            const data = JSON.parse(localStorage.getItem(key));
+            const tableContainer = document.getElementById('savedResults');
+            
+            if (data) {
+                const h2 = document.createElement('h2');
+                const h1 = document.createElement('h1');
+                h2.innerHTML = "Previously saved results from localstorage";
+                h1.innerHTML = data.length;
+                tableContainer.appendChild(h2);
+                tableContainer.appendChild(h1);
+                const table = document.createElement('table');
+                const thead = document.createElement('thead');
+                const tbody = document.createElement('tbody');
+
+                const headers = ['Project name', 'Manager name'];
+                const headerRow = document.createElement('tr');
+                headers.forEach(header => {
+                    const th = document.createElement('th');
+                    th.textContent = header;
+                    headerRow.appendChild(th);
+                });
+                thead.appendChild(headerRow);
+                table.appendChild(thead);
+
+                data.forEach(project => {
+                    const row = document.createElement('tr');
+                    Object.entries(project).forEach(([key, value]) => {
+                        if(key != "_id"){
+                            const cell = document.createElement('td');
+                            cell.textContent = value;
+                            row.appendChild(cell);
+                        }
+                    });
+                    tbody.appendChild(row);
+                });
+                table.appendChild(tbody);
+
+                tableContainer.appendChild(table);
+            } else {
+                tableContainer.textContent = 'There are no previously saved results.';
+            }
+        }
+        
+        const onLoad = () => {
+            showSavedResults(<?php echo("'$currentFile,$pr_manager'")?>);
+            saveToLocalStorage();
+        }
+        
+        window.onload = onLoad;
+    </script>
 </head>
 <body>
     <h2>The number of projects of the <?php echo($pr_manager)?></h2>
-    <h1><?php echo($count)?></h1>
+    <h1><?php echo(count($projects))?></h1>
 
     <?php
     if (count($projects) == 0) {
@@ -51,5 +111,7 @@ foreach ($cursor as $project) {
     <?php
     }
     ?>
+
+    <div id="savedResults" class="savedResults"></div>
 </body>
 </html>
