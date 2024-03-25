57a12b2eedf0cdb69113f47373fb7fa96a18103b
Bohdan <rogozyanskiy.bogdan@gmail.com>
2024-03-25 14:51:08 +0200

    refactor

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
 require_once __DIR__ . "/vendor/autoload.php";
 
-$collection_tasks = (new MongoDB\Client)->dbforlab->tasks;
-$collection_projects = (new MongoDB\Client)->dbforlab->projects;
+$client = new MongoDB\Client("mongodb://localhost:27017");
+
+$collection_tasks = $client->dbforlab->tasks;
+$collection_projects = $client->dbforlab->projects;
+
+$distinctManagers = $collection_projects->distinct("manager");
+$distinctProjects = $collection_projects->distinct("name");
 ?>
 
 <!DOCTYPE html>
@@ -15,10 +20,14 @@ $collection_projects = (new MongoDB\Client)->dbforlab->projects;
 </head>
 <body>
     <form method="post" action="submit_project_date.php">
-        <h2>Select completed tasks</h2>
-        <label for="project">Select a project:</label>
+        <h2>Completed tasks for the selected project on the specified date</h2>
+        <label for="project">Choose a project:</label>
         <select name="project" id="project">
-        
+        <?php
+            foreach ($distinctProjects as $project) {
+                echo "<option value='$project'>$project</option>";
+            }
+        ?>
         </select><br>
         <label for="date">Choose a date:</label>
         <input type="date" name="date" id="date"><br>
@@ -26,19 +35,27 @@ $collection_projects = (new MongoDB\Client)->dbforlab->projects;
     </form>
 
     <form method="post" action="submit_manager.php">
-        <h2>Choose the number of projects</h2>
+        <h2>The number of projects of the specified manager</h2>
         <label for="pr_manager">Choose the manager of the projects:</label>
         <select name="pr_manager" id="pr_manager">
-            <!--  -->
+            <?php
+                foreach ($distinctManagers as $Manager) {
+                    echo "<option value='$Manager'>$Manager</option>";
+                }
+            ?>
         </select><br>
         <input type="submit" value="Submit">
     </form>
 
     <form method="post" action="submit_workers.php">
-        <h2>Select employees</h2>
+        <h2>Employees under the leadership of an selected manager</h2>
         <label for="manager">Choose the manager of the employee:</label>
         <select name="manager" id="manager">
-            <!--  -->
+        <?php
+            foreach ($distinctManagers as $manager) {
+                echo "<option value='$manager'>$manager</option>";
+            }
+        ?>
         </select><br>
         <input type="submit" value="Submit">
     </form>
