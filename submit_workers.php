4a77a36462c26428fe7be729c797b0b1f8455026
Bohdan <rogozyanskiy.bogdan@gmail.com>
2024-03-26 01:11:18 +0200

    Employees under the leadership localstorage done

--- submit_workers.php
+++ submit_workers.php
@@ -17,6 +17,59 @@ $workers = $collection_tasks->distinct("workers", $query, ["_id" => 0]);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo($pr_manager)?></title>
     <link rel="stylesheet" href="./css/response.css">
+    <script>
+        const saveToLocalStorage = () => {
+            const key = <?php echo("'$currentFile,$pr_manager'")?>;
+            const data = <?php echo(json_encode($workers))?>;
+            if(data.length != 0){
+                localStorage.setItem(key, JSON.stringify(data));
+            }
+        }
+        
+        const showSavedResults = (key) => {
+            const data = JSON.parse(localStorage.getItem(key));
+            const tableContainer = document.getElementById('savedResults');
+            
+            if (data) {
+                const h2 = document.createElement('h2');
+                h2.innerHTML = "Previously saved results from localstorage";
+                tableContainer.appendChild(h2);
+                const table = document.createElement('table');
+                const thead = document.createElement('thead');
+                const tbody = document.createElement('tbody');
+
+                const headers = ['Employees name'];
+                const headerRow = document.createElement('tr');
+                headers.forEach(header => {
+                    const th = document.createElement('th');
+                    th.textContent = header;
+                    headerRow.appendChild(th);
+                });
+                thead.appendChild(headerRow);
+                table.appendChild(thead);
+
+                data.forEach(worker => {
+                    const row = document.createElement('tr');
+                    const cell = document.createElement('td');
+                    cell.textContent = worker;
+                    row.appendChild(cell);
+                    tbody.appendChild(row);
+                });
+                table.appendChild(tbody);
+
+                tableContainer.appendChild(table);
+            } else {
+                tableContainer.textContent = 'There are no previously saved results.';
+            }
+        }
+        
+        const onLoad = () => {
+            showSavedResults(<?php echo("'$currentFile,$pr_manager'")?>);
+            saveToLocalStorage();
+        }
+        
+        window.onload = onLoad;
+    </script>
 </head>
 <body>
     <h2>Employees under the leadership of an <?php echo($pr_manager)?></h2>
@@ -41,5 +94,7 @@ $workers = $collection_tasks->distinct("workers", $query, ["_id" => 0]);
     <?php
     }
     ?>
+
+    <div id="savedResults" class="savedResults"></div>
 </body>
 </html>
